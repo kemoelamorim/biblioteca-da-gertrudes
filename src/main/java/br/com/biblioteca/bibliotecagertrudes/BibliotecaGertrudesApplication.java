01787fd4f0a2c6526430554479a19605f9b29664
Kemoel Amorim Miranda <kemoel.amorim@workdb.com.br>
2023-04-15 19:26:30 -0300

    projeto spring

---
+++ src/main/java/br/com/biblioteca/bibliotecagertrudes/BibliotecaGertrudesApplication.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca.bibliotecagertrudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaGertrudesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaGertrudesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/biblioteca/bibliotecagertrudes/BibliotecaGertrudesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca.bibliotecagertrudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaGertrudesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
